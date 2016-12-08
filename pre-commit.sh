4cbf22e26f029020e60adebbdb6d7352c94a37a2
renat2985 <wul@inbox.lv>
2016-12-09 00:05:03 +0200

    Датчик температуры и влажности

--- pre-commit.sh
+++ pre-commit.sh
@@ -9,4 +9,10 @@ mv dev_data/js/*.js.gz data/js
 mv dev_data/css/*.css.gz data/css
 mv dev_data/lang/*.json.gz data/lang
 
-test -f *.generic.bin && cp *.generic.bin build/build.generic_flash_size_1Mb.254Kb_`date '+%G.%m.%d'`.bin
+#test -f *.generic.bin && cp *.generic.bin build/build.generic_flash_size_1Mb.254Kb_`date '+%G.%m.%d'`.bin
+FILE_SUM=`md5sum *.generic.bin`
+COPY_SUM=`md5sum ./build/$(ls -lt ./build | head -n2 |tail -n1 | awk '{print $9}')`
+if [ "$FILE_SUM" != "$COPY_SUM" ]
+then
+cp *.generic.bin build/build.generic_flash_size_1Mb.254Kb_`date '+%G.%m.%d'`.bin
+fi
