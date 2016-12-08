dbc80c5c9928ee481c15f76f189322691486dbc5
renat2985 <wul@inbox.lv>
2016-12-08 11:18:16 +0200

    Creale build folder

--- pre-commit.sh
+++ pre-commit.sh
@@ -8,3 +8,5 @@ mv dev_data/*.htm.gz data/
 mv dev_data/js/*.js.gz data/js
 mv dev_data/css/*.css.gz data/css
 mv dev_data/lang/*.json.gz data/lang
+
+test -f *.generic.bin && cp *.generic.bin build/build.generic_flash_size_1Mb.254Kb_`date '+%G.%m.%d'`.bin
