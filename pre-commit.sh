e99524a2784a95ebdbac0b4b0967f59899229c52
renat2985 <wul@inbox.lv>
2017-01-18 09:01:53 +0200

    Оптимизация

--- pre-commit.sh
+++ pre-commit.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
+cat dev_data/css/bootstrap.min.css dev_data/css/style.css > dev_data/css/build.css
+
 gzip -vrkf dev_data/*.htm
 gzip -vrkf dev_data/css/*.css
 gzip -vrkf dev_data/js/*.js
@@ -8,6 +10,8 @@ mv dev_data/*.htm.gz data/
 mv dev_data/js/*.js.gz data/js
 mv dev_data/css/*.css.gz data/css
 mv dev_data/lang/*.json.gz data/lang
+
+rm dev_data/css/build.css
 
 #test -f *.generic.bin && cp *.generic.bin build/build.generic_flash_size_1Mb.254Kb_`date '+%G.%m.%d'`.bin
 FILE_SUM=`md5sum Sonoff_WiFi_switch.ino.generic.bin`
