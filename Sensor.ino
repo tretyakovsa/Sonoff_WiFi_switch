4cf08f12db3b60c5db071b757456251dcac628c9
renat2985 <wul@inbox.lv>
2017-02-22 21:52:41 +0200

    add new chart page

--- Sensor.ino
+++ Sensor.ino
@@ -1,52 +1,26 @@
 void dht_init(){
-   HTTP.on("/sensor.json", handle_sensor);               //
-   dht.setup(DHTPIN); // data pin 2
-   Serial.println("DHT");
-   Serial.println(dht.getModel());
-  }
+ HTTP.on("/sensor.json", handle_sensor);
+ dht.setup(DHTPIN);
+ Serial.println("DHT");
+ Serial.println(dht.getModel());
+}
 
 void handle_sensor() {
-  String root = "{}";  // Формировать строку для отправки в браузер json формат
-  //{"SSDP":"SSDP-test","ssid":"home","ssidPass":"i12345678","ssidAP":"WiFi","ssidApPass":"","ip":"192.168.0.101"}
-  // Резервируем память для json обекта буфер может рости по мере необходимти, предпочтительно для ESP8266
-  DynamicJsonBuffer jsonBuffer;
-  //  вызовите парсер JSON через экземпляр jsonBuffer
-  JsonObject& json = jsonBuffer.parseObject(root);
-  // Заполняем поля json
-
-  String temp = "";
-  temp += dht.getTemperature();
-  json["Temperature"] = temp;
-  temp = "";
-  temp += dht.getHumidity();
-  json["Humidity"] = temp;
-  json["type"] = dht.getModel();
-    // Помещаем созданный json в переменную root
-  root="";
-  json.printTo(root);
-  HTTP.send(200, "text/json", root);
+ String root = "{}";  // Формировать строку для отправки в браузер json формат
+ //{"SSDP":"SSDP-test","ssid":"home","ssidPass":"i12345678","ssidAP":"WiFi","ssidApPass":"","ip":"192.168.0.101"}
+ // Резервируем память для json обекта буфер может рости по мере необходимти, предпочтительно для ESP8266
+ DynamicJsonBuffer jsonBuffer;
+ //  вызовите парсер JSON через экземпляр jsonBuffer
+ JsonObject& json = jsonBuffer.parseObject(root);
+ // Заполняем поля json
+ JsonArray& data = json.createNestedArray("data");
+ data.add(dht.getTemperature());  // 6 is the number of decimals to print
+ JsonArray& data2 = json.createNestedArray("data2");
+ data2.add(dht.getHumidity());  // 6 is the number of decimals to print
+ json["type"] = dht.getModel();
+ json["points"] = "10";
+ // Помещаем созданный json в переменную root
+ root="";
+ json.printTo(root);
+ HTTP.send(200, "text/json", root);
 }
-
- /*
-  String json = "\"Temperature\":\"";
-  String temp = "";
-  temp += dht.getTemperature();
-  if (temp == "nan") {
-    json += "hidden";
-  } else {
-    json += temp;
-  }
-  // Влажность
-  json += "\",\"Humidity\":\"";
-  String humi = "";
-  humi += dht.getHumidity();
-  if (humi == "nan") {
-    json += "hidden";
-  } else {
-    json += humi;
-  }
-
-  HTTP.send(200, "text/json", "[{"+json+"\"}]");
-}
-
-*/
