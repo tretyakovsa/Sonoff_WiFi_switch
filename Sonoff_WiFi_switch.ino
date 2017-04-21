784d6db7c33dc4c432eb9db472550b39cbbea371
renat2985 <wul@inbox.lv>
2017-04-21 09:58:38 +0300

    Финальный фикс обновления по http

--- Sonoff_WiFi_switch.ino
+++ Sonoff_WiFi_switch.ino
@@ -158,7 +158,7 @@ void setup() {
   Serial.println(chipID);
   FS_init();         // Включаем работу с файловой системой
   loadConfig();      // Загружаем настройки из файла
-  //initRGB();
+  initRGB();
   WiFi_init();       //Запускаем WIFI
   HTTP_init();       //настраиваем HTTP интерфейс
   SSDP_init();       //запускаем SSDP сервис
